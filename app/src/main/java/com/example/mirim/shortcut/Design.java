1ac1dad1dffc18f90dc690b856769738154973f3
Eunju Lee <ljhi0222@naver.com>
2018-06-21 20:50:58 +0900

    은주가 만든 부분

---
+++ app/src/main/java/com/example/mirim/shortcut/Design.java
@@ -0,0 +1,19 @@
+package com.example.mirim.shortcut;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Design extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.design, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/mirim/shortcut/Develop.java
@@ -0,0 +1,18 @@
+package com.example.mirim.shortcut;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Develop  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.develop, container, false);
+
+        return rootView;
+    }
+}
