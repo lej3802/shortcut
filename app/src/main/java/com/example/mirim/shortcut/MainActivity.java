3102af067fe80a6170992969289805f35f7e0057
sunny <sunnych0214@gmail.com>
2018-06-23 20:49:53 +0900

    데이터 다 넣었다

--- app/src/main/java/com/example/mirim/shortcut/MainActivity.java
+++ app/src/main/java/com/example/mirim/shortcut/MainActivity.java
@@ -80,12 +80,16 @@ public class MainActivity extends AppCompatActivity {
         DB = openOrCreateDatabase("data", Context.MODE_PRIVATE, null);
         dbresult+="db cre, ";
 
-        //테이블 생성
-        if(DB.isOpen()){
+        //테이블 열렸으면 삭제
+        /*if(DB.isOpen()){
             String sql = "drop table data";
             DB.execSQL(sql);
             dbresult+="tab drop, ";
         }
+        */
+
+
+        //테이블 생성
         String sql = "create table if not exists " + "data (" +
                     "id integer primary key autoincrement, " +
                     "pgname text," +
@@ -221,6 +225,72 @@ public class MainActivity extends AppCompatActivity {
         DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvVs', 'Alt+F8;', '인덴트 정리',0);");
         DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvVs', 'Ctrl+K', '주석 처리/해제',0);");
         DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvVs', 'Ctrl+C/U', '선택 주석 처리/해제',0);");
+
+
+        // 에디트 플러스
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+N', '새문서 작성',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+Shift+N', '새 HTML 페이지 작성',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+O', '파일 열기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+S', '저장하기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+End', '문서의 끝으로 이동',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+Shift+Delete', '현재 줄을 끝까지 지움',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+F', '찾기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+Y', '이전 취소 동작 다시 수행',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('DvEd', 'Ctrl+H', '문자열을 찾아 바꿈',0);");
+
+        // 한글
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+S', '저장하기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+Y', '한 줄 지우기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Alt+N', '새 문서 만들기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+Alt+T', '새 탭 만들기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+Alt+N', '문서마당',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Alt+O', '불러오기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Alt+Y', '다른 이름으로 저장하기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+Q', '문서정보',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Alt+P', '인쇄',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+F4', '문서 닫기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'F7', '편집용지 설정',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+Z', '문서의 상태를 전 상태로 되돌림',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+X', '문서의 글이나 그림을 잘라냄',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+C', '문서의 글이나 그림을 복사',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Alt+C', '문서의 글이나 그림의 속성을 복사',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+G', '문서를 전체화면으로 바꿈',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Ctrl+W', '글을 아이콘으로 바꿈',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'F9', '한글을 한자로 변환',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Alt+F9', '한자를 한글로 변환',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoHn', 'Alt+T', '문단 모양',0);");
+
+        // 엑셀
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'Ctrl+enter', '범위 이동',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'Ctrl+F5', '창 사이즈 변환',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'Ctrl+D', '윗 셀 복사 후 붙여넣기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'F2', '현재 셀 편집',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'Shift+F9', '시트 재계산',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', ' Shift+F3', '함수 마법사',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'Alt+enter', '한 셀에 여러 줄 입력',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'Alt+F2', '다른 이름으로 저장',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoEx', 'Alt+F11', 'VB 편집기 실행',0);");
+
+        // 파워포인트
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+A', '모두 선택',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+B', '굵은 글씨',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+C', '복사하기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+G', '그룹지정',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+H', '텍스트 변경',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Alt+K', '하이퍼링크',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+N', '새 슬라이드 추가',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+O', '슬라이드 열기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+P', '인쇄',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+V', '붙여넣기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+F2', '인쇄 미리보기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+[/]', '글씨 크기 조절',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+X', '잘라내기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Shift+F3', '눈금선 보기',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Ctrl+Shift+S', '다른 이름으로 저장',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'F7', '맞춤법 검사',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'F5', '슬라이드 쇼 실행',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'Shift+F5', '현재 슬라이드 쇼부터 시작',0);");
+        DB.execSQL("insert into " + "data" + "(pgname, shortkey, dowhat,star) values ('WoPt', 'end', '마지막 슬라이드로 이동',0);");
 
 
         return "rec ok, ";
