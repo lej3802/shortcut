0c839eff9df0eb7a0eef5eec5e9c0be515462f5a
sunny <sunnych0214@gmail.com>
2018-06-25 11:54:01 +0900

    리스트뷰에 커스텀 어댑터 만들어서 붙였다

--- app/src/main/java/com/example/mirim/shortcut/Send.java
+++ app/src/main/java/com/example/mirim/shortcut/Send.java
@@ -1,11 +1,15 @@
 package com.example.mirim.shortcut;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -15,10 +19,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Send extends AppCompatActivity{
-    private ArrayList<HashMap<String,String>> Data = new ArrayList<HashMap<String, String>>();
+    /*private ArrayList<HashMap<String,String>> Data = new ArrayList<HashMap<String, String>>();
     private HashMap<String,String> InputData1 = new HashMap<>();
     private HashMap<String,String> InputData2 = new HashMap<>();
-    private ListView listView;
+    private ListView listView;*/
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,57 @@ public class Send extends AppCompatActivity{
         setContentView(R.layout.send);
 
         Intent intent = getIntent();
-
         String name = intent.getExtras().getString("key");
 
-        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        ListView listview ;
+        ListViewAdapter adapter;
+
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.list_view);
+        listview.setAdapter(adapter);
+
+
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.round_favorite_black_18dp),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.round_favorite_black_18dp),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.round_favorite_black_18dp),
+                "Ind", "Assignment Ind Black 36dp") ;
+
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position) ;
+
+                String titleStr = item.getTitle() ;
+                String descStr = item.getDesc() ;
+                Drawable iconDrawable = item.getIcon() ;
+
+
+                // TODO : use item data.
+            }
+        }) ;
+
+
+
+
+
+
+
+
+
+
+
+        //Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
 
         /*
         listView =(ListView)findViewById(R.id.list_view);
